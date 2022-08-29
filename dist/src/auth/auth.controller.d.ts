d86c145149c34333c50d79971c40f410991de44f
Long Nguyen <shenlong1991@gmail.com>
2022-08-29 11:23:05 +0700

    Delete dist directory

--- dist/src/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    signUp(body: AuthDto): Promise<import(".prisma/client").User>;
-    signIn(body: AuthDto): Promise<import(".prisma/client").User>;
-}
--- dist/src/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from "../prisma/prisma.service";
-import { AuthDto } from "./dto";
-export declare class AuthService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    signUp(dto: AuthDto): Promise<import(".prisma/client").User>;
-    signIn(dto: AuthDto): Promise<import(".prisma/client").User>;
-}
