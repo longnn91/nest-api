684190af84106843243b0bb17addd84848418192
Long Nguyen <shenlong1991@gmail.com>
2022-08-30 08:59:37 +0700

    Delete dist directory

--- dist/src/auth/strategy/jwt.strategy.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from "@nestjs/config";
-declare const JwtStrategy_base: new (...args: any[]) => any;
-export declare class JwtStrategy extends JwtStrategy_base {
-    constructor(config: ConfigService);
-}
-export {};
